7105dd28f31d52e980c4184bd7d9594ff60262a3
Jhonata Peres <jhonatapers@gmail.com>
2021-03-07 13:58:58 -0300

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelicateBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
